6b4612f4a30c8a11ffbde98560d4b62d3faf25f0
Yohans Nascimento <yohans.dev@gmail.com>
2024-02-10 10:05:14 -0300

    initial commit

---
+++ src/main/java/dev/yohans/NewsletterSystemApiApplication.java
@@ -0,0 +1,13 @@
+package dev.yohans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yohans/NewsletterSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yohans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
