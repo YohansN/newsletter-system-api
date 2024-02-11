395097c025fb896623ed0bd8dfd35c38c8ccff24
Yohans Nascimento <yohans.dev@gmail.com>
2024-02-11 17:42:01 -0300

    feat: new subscribers query method

--- src/main/java/dev/yohans/repositories/SubscriberRepository.java
+++ src/main/java/dev/yohans/repositories/SubscriberRepository.java
@@ -2,7 +2,13 @@ package dev.yohans.repositories;
 
 import dev.yohans.models.Subscriber;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SubscriberRepository extends JpaRepository<Subscriber, Integer> { }
+public interface SubscriberRepository extends JpaRepository<Subscriber, Integer> {
+    @Query("SELECT s.email FROM Subscriber s WHERE s.isActive = true")
+    List<String> findAllByIsActiveTrue();
+}
